9e0ef347ad8bc1e3bdae8c551f0eac6c6dad01b8
anshita14garg@gmail.com <anshita14garg@gmail.com>
2026-06-04 21:38:39 +0530

    Experiment

---
+++ Exp7/src/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard(name , age, marks) {
+    return (
+        <div classname="student-card">
+            <h2>Student card</h2>
+            <p> This is a simple Student Card Component.</p>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Marks: {marks}</p> 
+        </div>
+    )
+}
